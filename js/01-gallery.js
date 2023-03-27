46192084d241044221291a7e1890e33ef7ce43d7
Ihor-M28 <114598607+Ihor-M28@users.noreply.github.com>
2023-03-28 00:14:23 +0300

    new
    new

---
+++ js/01-gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.addEventListener("click", onClickGalleryItem);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+      
+function onClickGalleryItem(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return
+  }
+    
+const instance = basicLightbox.create(`
+    <img src= ${evt.target.dataset.source} width="800" height="600">
+`)
+
+instance.show()
+  }
---
+++ js/02-lightbox.js
@@ -0,0 +1,31 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.addEventListener("click", onClickGalleryItem);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
+      
+function onClickGalleryItem(evt) {
+    evt.preventDefault();
+}
+var lightbox = new SimpleLightbox(".gallery__link", { captionsData: 'alt', captionDelay: 250, });
+  
+
